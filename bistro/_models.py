3f9a437b7e28b5418ebcd98dcc5de5ff95c9dd7e
ozefe <gh@efe.cv>
2023-09-04 00:44:16 +0300

    Added notification-related models
    - Also minor improvements to the language used in docstrings
    
    Signed-off-by: ozefe <gh@efe.cv>

--- bistro/_models.py
+++ bistro/_models.py
@@ -87,25 +87,25 @@ class UserSubscription:
     subscription's ID, price, name, and more. For complete data, refer to the :class:`Subscription`.
 
     Keyword Arguments:
-        id: The unique identifier of the subscription.
-        ref_id: The reference ID of the subscription.
-        price: The subscription price in Turkish Lira (₺).
-        available_period: The subscription period in months.
-        category_code: The category code of the subscription.
-        group_code: The group code of the subscription.
-        subcategory_code: The subcategory code of the subscription.
-        product_type_id: The product type ID of the subscription.
-        profile_id: The associated profile ID.
-        name: The name of the subscription in Turkish.
-        name_en: The name of the subscription in English.
-        created_timestamp: The creation timestamp in milliseconds.
-        expiration_timestamp: The expiration timestamp in milliseconds or microseconds.
-        created_date_text: The creation date in the `DD.MM.YYYY` format.
-        expiration_date_text: The expiration date in the `DD.MM.YYYY` format.
+        id: Unique identifier of the subscription.
+        ref_id: Reference ID of the subscription.
+        price: Subscription price in Turkish Lira (₺).
+        available_period: Subscription period in months.
+        category_code: Category code of the subscription.
+        group_code: Group code of the subscription.
+        subcategory_code: Subcategory code of the subscription.
+        product_type_id: Product type ID of the subscription.
+        profile_id: Associated profile ID.
+        name: Name of the subscription in Turkish.
+        name_en: Name of the subscription in English.
+        created_timestamp: Creation timestamp in milliseconds.
+        expiration_timestamp: Expiration timestamp in milliseconds or microseconds.
+        created_date_text: Creation date in the `DD.MM.YYYY` format.
+        expiration_date_text: Expiration date in the `DD.MM.YYYY` format.
 
     Attributes:
-        created_datetime: The calculated creation date and time.
-        expiration_datetime: The calculated expiration date and time.
+        created_datetime: Calculated creation date and time.
+        expiration_datetime: Calculated expiration date and time.
 
     Raises:
         InvalidTimestampError: If provided timestamp is not convertible to a valid datetime.
@@ -185,14 +185,14 @@ class User:
     This class encapsulates information about a user, including identification, username, and email.
 
     Keyword Arguments:
-        id: The unique identifier of the user.
-        username: The username of the user.
-        email: The email address associated with the user.
+        id: Unique identifier of the user.
+        username: Username of the user.
+        email: Email address associated with the user.
         email_confirmed: Indicates if the user's email has been confirmed.
         password_change_required: Indicates if a password change is required.
         endeks_notification_read: Indicates if the user has read the Endeks notification.
-        state: The state or status of the user.
-        family_name: The family name of the user.
+        state: State or status of the user.
+        family_name: Family name of the user.
         subscriptions: List of :class:`UserSubscription` instances for user's subscriptions.
 
     Notes:
@@ -244,21 +244,21 @@ class LoginSession:
     Login session objects returned whenever we check our current login status or login with user credentials.
 
     Keyword Arguments:
-        id: The unique identifier of the login session.
+        id: Unique identifier of the login session.
         user: Logged in :class:`User` object.
-        state: The state or status of the login session.
+        state: State or status of the login session.
         new_user: Indicates whether the user is newly registered.
         access_token_text: UUID version 4 access token as string.
         login_ip_text: IPv4 address as string.
-        created_timestamp: The creation timestamp in milliseconds.
-        expiration_timestamp: The expiration timestamp in milliseconds or microseconds.
+        created_timestamp: Creation timestamp in milliseconds.
+        expiration_timestamp: Expiration timestamp in milliseconds or microseconds.
 
     Attributes:
         access_token: :class:`uuid.UUID` object representing UUID version 4 access token.
         login_ip: :class:`ipaddress.IPv4Address` object representing IP version 4 address that the login session was
             initiated with.
-        created_datetime: The calculated creation date and time.
-        expiration_datetime: The calculated expiration date and time.
+        created_datetime: Calculated creation date and time.
+        expiration_datetime: Calculated expiration date and time.
 
     Raises:
         UUIDError: If :attr:`access_token_text` is not a valid UUID version 4 token.
@@ -319,6 +319,7 @@ class LoginSession:
     expiration_datetime: datetime.datetime = dataclasses.field(init=False)
 
     def __post_init__(self) -> None:
+        """Initialize calculated fields."""
         try:
             object.__setattr__(self, 'access_token', uuid.UUID(self.access_token_text))
         except ValueError as tb:
@@ -334,3 +335,111 @@ class LoginSession:
 
         object.__setattr__(self, 'expiration_datetime',
                            _timestamp_to_datetime(self.expiration_timestamp / 1_000.0, do_overflow=False))
+
+
+@dataclasses.dataclass(frozen=True, kw_only=True, slots=True, weakref_slot=True)
+class Notification:
+    """Represents a notification
+
+    Keyword Arguments:
+        id: Unique identifier of the notification.
+        type: Type of the notification.
+        state: Read/unread state of the notification.
+        title: Turkish title or header text of the notification.
+        title_en: English title or header text of the notification.
+        content: Turkish content text of the notification.
+        content_en: English content text of the notification.
+        created_timestamp: Creation timestamp in milliseconds.
+
+    Attributes:
+        created_datetime: Calculated creation date and time.
+
+    Raises:
+        InvalidTimestampError: If provided timestamp is not convertible to a valid datetime.
+        OverflowTimestampError: If :attr:`created_timestamp` is larger than 32-bit integer.
+
+    Notes:
+        - This class is intended to be used as an immutable data container, hence the `frozen` attribute.
+        - Use the `slots` attribute for optimized memory usage.
+        - The `weakref_slot` attribute enables weak references.
+
+    See Also:
+        - :class:`InvalidTimestampError`
+        - :class:`OverflowTimestampError`
+        - :class:`NotificationPage`
+
+    Example:
+        >>> Notification(
+        ...     id=7664950,
+        ...     type='NEW_PRODUCT',
+        ...     state='READ',
+        ...     title='Yeni Ürün',
+        ...     title_en='New Product',
+        ...     content='PP Piyasa Verileri Aboneliği (12 Ay)',
+        ...     content_en='Market Data Subscription (12 Months)',
+        ...     created_timestamp=1693615981840
+        ... )
+    """
+    id: int
+    type: str
+    state: str
+    title: str
+    title_en: str
+    content: str
+    content_en: str
+    created_timestamp: int = dataclasses.field(repr=False)
+    created_datetime: datetime.datetime = dataclasses.field(init=False)
+
+    def __post_init__(self) -> None:
+        """Initialize calculated datetime field."""
+        object.__setattr__(self, 'created_datetime', _timestamp_to_datetime(self.created_timestamp / 1_000.0))
+
+
+@dataclasses.dataclass(frozen=True, kw_only=True, slots=True, weakref_slot=True)
+class NotificationPage:
+    """Represents a notification page that usually holds multiple notifications within
+
+    Keyword Arguments:
+        index: Index of the notification page.
+        items_count: Total count of the notifications within.
+        notifications: List of :class:`Notification` objects.
+
+    Notes:
+        - This class is intended to be used as an immutable data container, hence the `frozen` attribute.
+        - Use the `slots` attribute for optimized memory usage.
+        - The `weakref_slot` attribute enables weak references.
+
+    See Also:
+        - :class:`Notification`
+
+    Example:
+        >>> NotificationPage(
+        ...     index=1,
+        ...     items_count=10,
+        ...     notifications= [
+        ...         Notification(
+        ...             id=7664950,
+        ...             type='NEW_PRODUCT',
+        ...             state='READ',
+        ...             title='Yeni Ürün',
+        ...             title_en='New Product',
+        ...             content='PP Piyasa Verileri Aboneliği (12 Ay)',
+        ...             content_en='Market Data Subscription (12 Months)',
+        ...             created_timestamp=1693615981840
+        ...         ),
+        ...         Notification(
+        ...             id=7664808,
+        ...             type='NEW_PRODUCT',
+        ...             state='READ',
+        ...             title='Yeni Ürün',
+        ...             title_en='New Product',
+        ...             content='PP Pay Bazında Veriler Aboneliği (12 Ay)',
+        ...             content_en='Equity Based Data Subscription (12 Months)',
+        ...             created_timestamp=1693615970100
+        ...         )
+        ...     ]
+        ... )
+    """
+    index: int
+    items_count: int
+    notifications: list[Notification]
